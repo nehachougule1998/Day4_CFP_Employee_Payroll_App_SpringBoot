f7f2a9850937969bcfc50172ed845e439c454357
nehachougule1998 <nchougule734@gmail.com>
2022-07-22 12:23:56 +0530

    Validation

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeePayrollDTO.java
@@ -1,6 +1,13 @@
 package com.bridgelabz.employeepayrollapp.DTO;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+    @Pattern(regexp = "^[A-Z]{1}[A-Za-z\\s]{1,}$",message = "Employee name can not be null")
     public String name;
+
+    @Min(value = 500, message = "Salary should be more than 500")
     public int salary;
 
     public EmployeePayrollDTO(String name, int salary) {
